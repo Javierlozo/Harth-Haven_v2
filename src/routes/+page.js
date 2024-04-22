0c4f7d4d518fed06cd0e96cf1166e6771430d71d
Luis Lozoya <luis.lozoya@querri.com>
2024-04-22 10:41:02 -0400

    Added prerender false and ssr false

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,11 +1,12 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-export const prerender = true;
+export const prerender = false;
+export const ssr = false;
 
 import { cardsData } from '../lib/data/cards_data';
 
 export function load() {
-	return {
-		homes: cardsData
-	};
+  return {
+    homes: cardsData,
+  };
 }
